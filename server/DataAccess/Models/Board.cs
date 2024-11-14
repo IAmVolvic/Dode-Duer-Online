7d3d3bbeaeb1baf9ea694a13763d58432b2a1cf0
Kimi310 <94898434+Kimi310@users.noreply.github.com>
2024-11-14 15:16:31 +0100

    dokcer plus context

---
+++ server/DataAccess/Models/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class Board
+{
+    public Guid Id { get; set; }
+
+    public Guid Userid { get; set; }
+
+    public Guid Gameid { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateOnly Dateofpurchase { get; set; }
+
+    public virtual ICollection<Chosennumber> Chosennumbers { get; set; } = new List<Chosennumber>();
+
+    public virtual Game Game { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ server/DataAccess/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class Transaction
+{
+    public Guid Id { get; set; }
+
+    public Guid Userid { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string Mobilepayid { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
