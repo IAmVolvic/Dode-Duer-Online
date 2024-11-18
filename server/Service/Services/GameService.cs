5c6e30a028589312dbaead58cd0a74a99fdb13b8
Kimi310 <94898434+Kimi310@users.noreply.github.com>
2024-11-18 17:05:31 +0100

    new game can be created

---
+++ server/Service/Services/GameService.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
+using Service.Services.Interfaces;
+using Service.TransferModels.Responses;
+
+namespace Service.Services;
+
+public class GameService(IGameRepository gameRepository) : IGameService
+{
+    public GameResponseDTO NewGame(int prize)
+    {
+        var game = new Game();
+
+        // Get the current date
+        var today = DateTime.Now;
+
+        // Calculate the last Monday
+        int daysSinceMonday = (int)today.DayOfWeek - (int)DayOfWeek.Monday;
+        if (daysSinceMonday < 0)
+        {
+            daysSinceMonday += 7; // Adjust for Sundays
+        }
+        var lastMonday = today.AddDays(-daysSinceMonday);
+
+        // Set the game's date to the last Monday
+        game.Id = Guid.NewGuid();
+        game.Date = DateOnly.FromDateTime(lastMonday);
+        game.Prizepool = prize;
+        game.Status = "Active";
+        
+        var activeGame = gameRepository.GetActiveGame();
+        if (activeGame != null)
+        {
+            activeGame.Status = "Inactive";
+        }
+        
+        var gameResponse = new GameResponseDTO().FromGame(gameRepository.NewGame(game,activeGame));
+        return gameResponse;
+    }
+}
---
+++ server/Service/TransferModels/Responses/GameResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Models;
+
+namespace Service.TransferModels.Responses;
+
+public class GameResponseDTO
+{
+    public Guid Id { get; set; }
+    public DateOnly Date { get; set; }
+    public string Status { get; set; }
+
+    public GameResponseDTO FromGame(Game game)
+    {
+        return new GameResponseDTO()
+        {
+            Id = game.Id,
+            Date = game.Date,
+            Status = game.Status
+        };
+    }
+}
