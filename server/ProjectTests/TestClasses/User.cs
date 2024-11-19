a24e3450e22638fab7b03ccb7a533a61d82a76a2
IAmVolvic <71038869+IAmVolvic@users.noreply.github.com>
2024-11-19 19:07:53 +0100

    Edits to tests

--- server/ProjectTests/TestClasses/User.cs
+++ server/ProjectTests/TestClasses/User.cs
@@ -13,42 +13,49 @@ namespace ProjectTests.TestClasses;
 
 public class User
 {
-    private readonly PgCtxSetup<UserContext> _pgCtxSetup = new();
+    private readonly PgCtxSetup<UserContext> _pgCtxSetup;
     private readonly IJWTManager _jwtManager = new JWTManager();
     private readonly WebApplicationFactory<Program> _factory;
     
     public User()
     {
+        DotNetEnv.Env.Load();
         // Initialize the WebApplicationFactory with your application class (Program.cs or Startup.cs)
         _factory = new WebApplicationFactory<Program>();
-        Environment.SetEnvironmentVariable("DefaultConnection", _pgCtxSetup._postgres.GetConnectionString());
+        string connectionString = Environment.GetEnvironmentVariable("DefaultConnection")!;
+        _pgCtxSetup = new PgCtxSetup<UserContext>(connectionString);
     }
     
     
     [Fact]
     public async Task Signup()
     {
-        var createNewUser = new UserSignupRequestDTO()
+        var createNewUser = new
         {
-            Name = "John Doe",
-            Email = "john@doe.com",
-            Password = "password"
+            name = "John Doe",
+            email = "john@doe.com",
+            password = "password"
         };
-        
+            
         var client = _factory.CreateClient();
-        
-        // Sim a request and get response
+            
+        // Simulate a request and get response
         var response = await client.PostAsJsonAsync("/User/signup", createNewUser);
-        
-        // Make sure response code is OK ( 200 )
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception($"Signup failed: {response.StatusCode}");
+        }
+            
+        // Ensure response code is OK ( 200 )
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        
-        // Check the content
+            
+        // Check the content of the response
         var user = await response.Content.ReadFromJsonAsync<UserResponseDTO>();
-        
+            
         Assert.NotNull(user);
         Assert.True(Guid.TryParse(user.Id, out Guid userId), "The returned user ID is not a valid GUID.");
-        
+            
         Assert.NotNull(user.JWT);
         Assert.True((_jwtManager.IsJWTValid(user.JWT) != null), "The jwt is not valid.");
     }
