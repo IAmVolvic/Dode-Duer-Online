6ed1fe22ab3bfe56522539ca02ee877eb4bf2a50
IAmVolvic <71038869+IAmVolvic@users.noreply.github.com>
2024-12-06 18:19:32 +0100

    Update index.tsx

--- client/src/components/dialogs/userSettings/index.tsx
+++ client/src/components/dialogs/userSettings/index.tsx
@@ -1,8 +1,9 @@
 import { BaseDialog, DialogSizeEnum, IBaseDialog } from "..";
 import { Button, Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 import { useLogout } from "@hooks/authentication/useLogout";
-import { FiSettings, FiCreditCard, FiLayout, FiLogOut } from "react-icons/fi";
+import { FiSettings, FiCreditCard, FiLayout, FiLogOut, FiUsers, FiSliders, FiShoppingCart } from "react-icons/fi";
 import classNames from 'classnames';
+import { useAuth } from "@hooks/authentication/useAuthentication";
 
 // TABS
 import { AccountTabContent } from "./tabs/account";
@@ -10,6 +11,7 @@ import { BillingTabContent } from "./tabs/billing";
 import { AppearanceTabContent } from "./tabs/appearance";
 
 export const UserSettingsDialog = (props: IBaseDialog) => { 
+    const {user, isLoggedIn} = useAuth();
     const logout = useLogout();
 
     const handleLogout = () => {
@@ -36,6 +38,27 @@ export const UserSettingsDialog = (props: IBaseDialog) => {
                             <div className="flex justify-center items-center"> <FiLayout className="opacity-60" size={20} /> </div>
                             <div className=""> Appearance </div>
                         </Tab>
+
+                        {isLoggedIn && user?.role === 'Admin' && (
+                            <>
+                                <div className="border-t-0.05r border-base-content/50 my-3"></div>
+
+                                <Tab className={({selected}) => classNames("flex flex-row items-center gap-5 py-2.5 outline-none", selected ? 'border-r-0.25r !border-primary':'')}>
+                                    <div className="flex justify-center items-center"> <FiUsers className="opacity-60" size={20} /> </div>
+                                    <div className=""> Users </div>
+                                </Tab>
+
+                                <Tab className={({selected}) => classNames("flex flex-row items-center gap-5 py-2.5 outline-none", selected ? 'border-r-0.25r !border-primary':'')}>
+                                    <div className="flex justify-center items-center"> <FiSliders className="opacity-60" size={20} /> </div>
+                                    <div className=""> Games </div>
+                                </Tab>
+
+                                <Tab className={({selected}) => classNames("flex flex-row items-center gap-5 py-2.5 outline-none", selected ? 'border-r-0.25r !border-primary':'')}>
+                                    <div className="flex justify-center items-center"> <FiShoppingCart className="opacity-60" size={20} /> </div>
+                                    <div className=""> Transactions </div>
+                                </Tab>
+                            </>
+                        )}
                         
                         <Button onClick={handleLogout} className="flex flex-row items-center gap-5 py-2.5 outline-none absolute bottom-0 mb-3 text-error"> 
                             <div className="flex justify-center items-center"> <FiLogOut className="opacity-60" size={20} /> </div>
